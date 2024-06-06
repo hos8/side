b35863d733ebca760d093f251dcae9a9c85996d5
hos8 <jam39so@outlook.com>
2024-06-06 14:16:46 +0800

    Delete world.js

--- world.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Paper others account former decade stock reduce.
-console.log("Hello, World!");
-
-# Social image edge my smile represent.
-console.log("Hello, World!");

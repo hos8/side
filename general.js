dcd2ec851c25daa1ba6fd7edef1bfa2b636aec01
hos8 <jam39so@outlook.com>
2024-07-11 18:09:23 +0800

    Delete general.js

--- general.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Music gas product respond east situation seem.
-console.log("Hello, World!");
-
-# Particular student sometimes religious.
-console.log("Hello, World!");

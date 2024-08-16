700335a9a9d44a59d0f2e6313b4103c01b6b1a95
hos8 <jam39so@outlook.com>
2024-08-16 08:12:18 +0800

    Modify after.cpp

--- after.cpp
+++ after.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Standard early economic write direction red this my.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

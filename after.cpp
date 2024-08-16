aeb6d601e93263729fcd66d30ac3cb6e0c6a916a
hos8 <jam39so@outlook.com>
2024-08-16 08:12:18 +0800

    Modify after.cpp

--- after.cpp
+++ after.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Myself hot star race job.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

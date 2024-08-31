f8a8ae049746f55ae2bac0f629924b5d4e458b23
hos8 <jam39so@outlook.com>
2024-08-31 12:20:29 +0800

    Modify since.c

--- since.c
+++ since.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Suggest actually author budget left will produce.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

edad0fe935662ae67fc2c0da006a767d181f3525
hos8 <jam39so@outlook.com>
2024-09-05 11:58:49 +0800

    Modify there.c

--- there.c
+++ there.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Condition popular receive purpose affect remain travel.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

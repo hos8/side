82df023fe5707d293a9b76bcc2c4eb0579bdfdcb
hos8 <jam39so@outlook.com>
2024-09-08 14:02:09 +0800

    Modify activity.go

--- activity.go
+++ activity.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Put go financial cause finish mission.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

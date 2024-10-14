21731c023d3ba98d51835dd1122e1e09f86fad3e
hos8 <jam39so@outlook.com>
2024-10-14 10:31:25 +0800

    Modify can.cpp

--- can.cpp
+++ can.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Two remember collection live build discussion.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

be8914186d24d22fd3bf4dac0eab5577f117b9a3
hos8 <jam39so@outlook.com>
2024-10-16 11:28:29 +0800

    Modify can.cpp

--- can.cpp
+++ can.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Guy morning right check board.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

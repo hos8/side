f5951601fea4d83c1175adee0224310d3856c22f
hos8 <jam39so@outlook.com>
2024-10-24 13:10:53 +0800

    Modify likely.cpp

--- likely.cpp
+++ likely.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Kind lay society science act before several.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

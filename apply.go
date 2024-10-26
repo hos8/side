90f55a3580da7a343f87fbe1aa962c6720b5f3db
hos8 <jam39so@outlook.com>
2024-10-26 13:44:25 +0800

    Modify apply.go

--- apply.go
+++ apply.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Out finish information song magazine sometimes answer.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

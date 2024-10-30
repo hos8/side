a22cf9dab32eddf1273b54157ec5a144bdfb7ef9
hos8 <jam39so@outlook.com>
2024-10-30 15:48:28 +0800

    Modify research.c

--- research.c
+++ research.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Coach point game region concern order somebody enter.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

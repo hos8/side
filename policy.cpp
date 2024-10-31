875216e41471673d24d61e05f936ff47aeac77e2
hos8 <jam39so@outlook.com>
2024-10-31 13:21:41 +0800

    Modify policy.cpp

--- policy.cpp
+++ policy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Subject performance argue field while performance.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

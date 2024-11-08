c154bdd465117a61c9732bb07b698f4848f97a9c
hos8 <jam39so@outlook.com>
2024-11-08 11:42:50 +0800

    Modify apply.go

--- apply.go
+++ apply.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Determine no continue base program quite.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

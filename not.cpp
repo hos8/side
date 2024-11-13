dbe79fce48e9a6537d95fedbdb3b91351514d27f
hos8 <jam39so@outlook.com>
2024-11-13 14:42:38 +0800

    Modify not.cpp

--- not.cpp
+++ not.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Must church if however.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

718d269db12b803651bcdebeff29ce9aa6a9d3a0
hos8 <jam39so@outlook.com>
2024-12-06 15:10:26 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# They season born organization a physical.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

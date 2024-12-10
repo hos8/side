d3983b093be756a34ddc21daab9f4b553755797f
hos8 <jam39so@outlook.com>
2024-12-11 06:23:41 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Impact make key possible.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

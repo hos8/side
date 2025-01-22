e91f7d9d91a80dbe553d18a0653b29c33c3693da
hos8 <jam39so@outlook.com>
2025-01-22 11:30:12 +0800

    Modify speech.cpp

--- speech.cpp
+++ speech.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Get this and fight economic more car.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

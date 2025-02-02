b554ea3ea53bf8ddffea113546ec44bb42b5f851
hos8 <jam39so@outlook.com>
2025-02-02 10:24:45 +0800

    Modify speech.cpp

--- speech.cpp
+++ speech.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mouth room place majority detail.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

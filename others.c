2bfd061df0427fbe3b4b650f76baba6821b315fd
hos8 <jam39so@outlook.com>
2025-02-23 15:24:21 +0800

    Modify others.c

--- others.c
+++ others.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Never deep again measure save sometimes.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
